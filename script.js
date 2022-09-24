a23c68b07a914cdf7b4af25cebeb9fad419e9698
Fabián Ponce <maximohumano@gmail.com>
2022-09-24 16:50:01 -0500

    Implement turnOff();

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function turnOff() {
-  
+  const mainNumbers = document.getElementById('display');
+  const subtext = document.querySelectorAll('.subtext');
+
+  mainNumbers.innerText = '';
+  subtext.forEach(subtextContainer => subtextContainer.innerText = '');
 }
