e75eccd747bfe0d7f18bad27d6a559d1f554bc4a
Fabián Ponce <maximohumano@gmail.com>
2022-09-24 17:26:31 -0500

    Input all functions skeletons

--- script.js
+++ script.js
@@ -5,3 +5,55 @@ function turnOff() {
   mainNumbers.innerText = '';
   subtext.forEach(subtextContainer => subtextContainer.innerText = '');
 }
+
+function turnOn_ClearDisplay() {
+  
+}
+
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function getSquareRoot() {
+
+}
+
+function getPercentage() {
+
+}
+
+function getResult() {
+
+}
+
+function setFloatingPoint() {
+
+}
+
+function memoryAddition() {
+
+}
+
+function memorySubtraction() {
+
+}
+
+function getResult_ClearMemory() {
+
+}
+
+function setNumber() {
+
+}
