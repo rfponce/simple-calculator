08cdebd9444e7a02f2620d8f9f6f23316247d8f2
Fabián Ponce <maximohumano@gmail.com>
2022-10-01 00:04:39 -0500

    Fix a bug when a result overflows the display
    The result goes out of the limits of the display when the digits
    was more than 8 in values less than 0

--- script.js
+++ script.js
@@ -117,6 +117,7 @@ function operate() {
   }
   
   if (result > 99999999) result = result.toExponential(3).toString(); // Try to get the string shorter (less than 8 digits)
+  if (result < 0.99999999) result = result.toExponential(3).toString();
   if (result > 1.000e+15) result = 'NO SPACE'; // Shows "NO SPACE" because there no more space on display to show the digits
   LCD_Display.displayOnScreen(result);
 }
